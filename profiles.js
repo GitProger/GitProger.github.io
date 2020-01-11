56b6d1251746d8706a1e2905fca4972a54cd1df0
GitProger <51924999+GitProger@users.noreply.github.com>
2020-01-11 21:01:50 +0300

    Create profiles.js

---
+++ profiles.js
@@ -0,0 +1,12 @@
+
+twitter = {
+    name: "twitter.com",
+    site: "https://twitter.com/",
+    link: "https://twitter.com/GitProger",
+};
+
+vk = {
+    name: "vk.com",
+    site: "https://vk.com/",
+    link: "https://vk.com/1ksta",
+};
