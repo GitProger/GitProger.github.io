5d64507f53b8bbd8a7fb10b3281e19cda5841b66
GitProger <51924999+GitProger@users.noreply.github.com>
2020-01-11 21:01:14 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,12 +1,12 @@
-/// My profiles on differant sites here
 
 function getCode(pro) {
+     function quote(text) {
+         return "\"" + text + "\"";
+     }
      var code = "";
-     return code;
+     code += "<a href=" + quote(pro.link) + ">";
+     code += "<img class=\"favicon withtext\" alt=\"\" title=\"\" src=";
+     code += quote("http://www.google.com/s2/favicons?domain=" + pro.site)
+     code += " />" + pro.name + "</a><br />"
+     return "<td>" + code + "</td>";
 }
-
-twitter = {
-    site: "",
-    img: "",
-    nick: "",
-};
