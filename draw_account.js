c02f3cc7b1bc635027774191c170e6f148139917
GitProger <51924999+GitProger@users.noreply.github.com>
2020-01-11 21:27:47 +0300

    Update draw_account.js

--- draw_account.js
+++ draw_account.js
@@ -3,7 +3,7 @@ function getCode(pro) {
          return "\"" + text + "\"";
      }
      var code = "";
-     code += "<a href=" + quote(pro.link) + ">";
+     code += "<a href=" + quote(pro.link) + " class=\"darklink\">";
      code += "<img class=\"favicon withtext\" alt=\"\" title=\"\" src=";
      code += quote("http://www.google.com/s2/favicons?domain=" + pro.site)
      code += " />" + pro.name + "</a><br />"
