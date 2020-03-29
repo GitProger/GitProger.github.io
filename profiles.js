06d51e0f3d4fe4a480b7b8fd7fab0dc6c6329c42
GitProger <51924999+GitProger@users.noreply.github.com>
2020-03-29 21:49:55 +0300

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -1,12 +1,3 @@
-function getCode(pro) {
-     var code = "";
-     code += "<a href=\"" + pro.link + "\" class=\"darklink\">";
-     code += "<img class=\"favicon withtext\" alt=\"\" title=\"\" src=";
-     code += "\"http://www.google.com/s2/favicons?domain=" + pro.site;
-     code += "\" />" + pro.name + "</a><br />";
-     return code;
-}
-
 
 twitter = {
     name: "twitter.com",
@@ -28,6 +19,15 @@ github = {
 
 facebook = {
     name: "facebook.com",
-    site: "https://facebook.com/",
+    site: "https://www.facebook.com/",
     link: "https://www.facebook.com/profile.php?id=100049113921229",
 };
+
+function getCode(pro) {
+     var code = "";
+     code += "<a href=\"" + pro.link + "\" class=\"darklink\">";
+     code += "<img class=\"favicon withtext\" src=\"";
+     code += "http://www.google.com/s2/favicons?domain=" + pro.site + "\" />";
+     code += pro.name + "</a><br />";
+     return code;
+}
