daba6ae981d9a87c6df49eea77d69a72f8a9f669
GitProger <51924999+GitProger@users.noreply.github.com>
2020-03-31 11:15:19 +0300

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -1,27 +1,21 @@
-
-twitter = {
-    name: "twitter.com",
-    site: "https://twitter.com/",
-    link: "https://twitter.com/GitProger",
-};
-
-vk = {
-    name: "vk.com",
-    site: "https://vk.com/",
-    link: "https://vk.com/1ksta",
-};
-
-github = {
-    name: "github.com",
-    site: "https://github.com/",
-    link: "https://github.com/GitProger",
-};
-
-facebook = {
-    name: "facebook.com",
-    site: "https://www.facebook.com/",
-    link: "https://www.facebook.com/profile.php?id=100049113921229",
-};
+profiles = [{
+        name: "vk.com",
+        site: "https://vk.com/",
+        link: "https://vk.com/1ksta",
+    }, {
+        name: "github.com",
+        site: "https://github.com/",
+        link: "https://github.com/GitProger",
+    }, {
+        name: "twitter.com",
+        site: "https://twitter.com/",
+        link: "https://twitter.com/GitProger",
+    }, {
+        name: "facebook.com",
+        site: "https://www.facebook.com/",
+        link: "https://www.facebook.com/profile.php?id=100049113921229",
+    },
+];
 
 function getCode(pro) {
      var code = "";
