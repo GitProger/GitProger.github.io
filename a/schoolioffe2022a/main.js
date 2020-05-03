41c2a253184d64361d0ec5def0c9da7fceda06b1
GitProger <51924999+GitProger@users.noreply.github.com>
2020-05-03 18:40:26 +0300

    Update main.js

--- a/schoolioffe2022a/main.js
+++ a/schoolioffe2022a/main.js
@@ -19,8 +19,8 @@ var subjects = {
 
 function Item(type, sub, ex, link) {
     return {
-        "type": type,
-        "subject": sub,
+        "type": types[type],
+        "subject": subjects[sub],
         "link": link,
         "explan": ex,
     };
@@ -33,4 +33,23 @@ var items = [
     Item("У", "История", "Учебник по истории",      "http://www.belgtis.ru/images/obuch/pm/IstoriyaRossiiimirasdrevnejshihvremendokoncaXIXvekaklassZagladinNVidr.pdf"),
     Item("У", "Химия", "Задачник по химии",         "https://vk.com/doc572297054_543760232?hash=cd65ebfda1a3c873d4&dl=a22a98f48ac93d1ab9"),
 ];
+
+
+function updateList() {
+    var e = document.getElementById("sortby");
+    var list = document.getElementById("list");
+    var by = e.value;
+    items.sort((a, b) => a[by] > b[by] ? 1 : -1);
+    var prev = "";
+
+    for (var i = 0; i < items.length; i++) {
+        var cur = items[i];
+        if (prev !== cur)
+            list.innerHTML += "<hr>" + cur[by];
+        var code = "<a href=\"" + cur.link + "\">" + cur.explan + "</a>;
+        list.innerHTML += code + "<br>";
+        prev = items[i];
+    }
+}
+
 
